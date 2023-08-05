32a121997cebd5b23cb541fbe39e92f7082d1550
Asif <msasifiiuc@gmail.com>
2023-08-05 21:37:38 +0600

    2nd commit

---
+++ Module-22 JS Problem/type.js
@@ -0,0 +1,15 @@
+const country='Bangladesh';
+const age=52;
+const isIndependent=true;
+const student ={id:14, class:15, name: 'Asif'};
+const num=[12, 14, 16, 18, 20];
+function sum(num1, num2){
+    return num1+num2;
+}
+
+console.log(typeof country);
+console.log(typeof age);
+console.log(typeof isIndependent);
+console.log(typeof student);
+console.log(typeof num);
+console.log(typeof sum);
